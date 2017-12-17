3dd15d0d4d45b039d6a88e669e7ba803d7a3e651
Артур <Artur91425@gmail.com>
2017-12-18 02:14:32 +0300

    refactoring

--- GrimoireKeeper/GrimoireKeeper.lua
+++ GrimoireKeeper/GrimoireKeeper.lua
@@ -1,12 +1,11 @@
---[[---------------------------------------------------------------------------------
-	Initialization
-------------------------------------------------------------------------------------]]
-
 GrimoireKeeper = AceLibrary('AceAddon-2.0'):new('AceEvent-2.0', 'AceHook-2.1')
 
 function GrimoireKeeper:OnInitialize()
+	local _, class = UnitClass('player')
+	if class ~= 'WARLOCK' then return end
+	
 	GrimoireTable = {
-		-- [name] = {id by ranks}
+		-- [spell name] = {grimoire id by ranks}
 		-- Imp
 		['Firebolt'] 			= {nil,     '16302', '16316', '16317', '16318', '16319', '16320'},
 		['Blood Pact'] 			= {'16321', '16322', '16323', '16324', '16325'},
@@ -28,70 +27,48 @@ function GrimoireKeeper:OnInitialize()
 		['Spell Lock'] 			= {'16388', '16389'},
 		['Paranoia'] 			= {'16390'}
 	}
-	
-	local _, class = UnitClass('player')
-	if class ~= 'WARLOCK' then return end
-end
-
-function GrimoireKeeper:OnEnable()
-	self:SecureHook('MerchantFrame_UpdateMerchantInfo')
 	self:RegisterEvent('PET_BAR_UPDATE')
+	self:SecureHook('MerchantFrame_UpdateMerchantInfo')
 end
-
-function GrimoireKeeper:OnDisable()
-	self:UnregisterAllEvents()
-end
-
---[[---------------------------------------------------------------------------------
-	Event Handlers
-------------------------------------------------------------------------------------]]
 
 function GrimoireKeeper:PET_BAR_UPDATE()
-	GrimoireKeeperData = {}
 	local hasPetSpells, petToken = HasPetSpells()
 	if not hasPetSpells and petToken ~= 'DEMON' then return end
 	
+	GrimoireKeeperData = {}	
 	for i=1, SPELLS_PER_PAGE do
 		local name, rank = GetSpellName(i, BOOKTYPE_PET)
-		if not name then break end	
+		if not name then break end
 		_, _, rank = string.find(rank, '(%d)')
 		if not rank then rank = 1 end
-		rank = tonumber(rank)
 		if GetLocale() ~= 'enUS' then
 			name = AceLibrary('Babble-Spell-2.2'):GetReverseTranslation(name)
 		end
-		for i=1, rank do
+		for i=1, tonumber(rank) do
 			local GrimoireID = GrimoireTable[name][i]
-			table.insert(GrimoireKeeperData, GrimoireID)
+			if GrimoireID then
+				table.insert(GrimoireKeeperData, GrimoireID)
+			end
 		end
 	end
 end
 
---[[--------------------------------------------------------------------------------
-	Shared Functions
------------------------------------------------------------------------------------]]
-
-function GrimoireKeeper:MerchantFrame_UpdateMerchantInfo()	
+function GrimoireKeeper:MerchantFrame_UpdateMerchantInfo()
 	for i=1, MERCHANT_ITEMS_PER_PAGE do
-		local check = false
 		local index = (MerchantFrame.page - 1) * MERCHANT_ITEMS_PER_PAGE + i
 		if index <= GetMerchantNumItems() then
 			local link = GetMerchantItemLink(index)
-			if link then
-				local _, _, itemID = string.find(link, 'item:(%d+):%d+:%d+:%d+')
-			end
-			if table.getn(GrimoireKeeperData) ~= 0 then
-				for i=1, table.getn(GrimoireKeeperData) do
-					if itemID == GrimoireKeeperData[i] then check = true end
+			if not link then return end
+			local _, _, itemID = string.find(link, 'item:(%d+):%d+:%d+:%d+')
+			for k in pairs(GrimoireKeeperData) do
+				if itemID == GrimoireKeeperData[k] then
+					local itemButton = getglobal('MerchantItem'..i..'ItemButton')
+					local merchantButton = getglobal('MerchantItem'..i)
+					SetItemButtonNameFrameVertexColor(merchantButton, 0, 0.75, 0.75)
+					SetItemButtonSlotVertexColor(merchantButton, 0, 0.75, 0.75)
+					SetItemButtonTextureVertexColor(itemButton, 0, 0.65, 0.65)
+					SetItemButtonNormalTextureVertexColor(itemButton, 0, 0.65, 0.65)
 				end
-			end
-			if check == true then
-				local itemButton = getglobal('MerchantItem'..i..'ItemButton')
-				local merchantButton = getglobal('MerchantItem'..i)
-				SetItemButtonNameFrameVertexColor(merchantButton, 0, 0.75, 0.75)
-				SetItemButtonSlotVertexColor(merchantButton, 0, 0.75, 0.75)
-				SetItemButtonTextureVertexColor(itemButton, 0, 0.65, 0.65)
-				SetItemButtonNormalTextureVertexColor(itemButton, 0, 0.65, 0.65)
 			end
 		end
 	end

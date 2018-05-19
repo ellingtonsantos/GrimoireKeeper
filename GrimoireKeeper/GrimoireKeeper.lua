06aca4d384e210ed6f37ec804bfdf0dc2d64b29b
Lichery <Artur91425@gmail.com>
2018-05-19 23:29:05 +0300

    3.3
    rewrote the addon again, but without the use of Ace2 libraries.

--- GrimoireKeeper/GrimoireKeeper.lua
+++ GrimoireKeeper/GrimoireKeeper.lua
@@ -1,48 +1,44 @@
-GrimoireKeeper = AceLibrary('AceAddon-2.0'):new('AceEvent-2.0', 'AceHook-2.1')
+local _, class = UnitClass('player')
+if class ~= 'WARLOCK' then return end
+
+local GrimoireKeeper = CreateFrame('Frame')
 local petType
-
-function GrimoireKeeper:OnInitialize()
-	local _, class = UnitClass('player')
-	if class ~= 'WARLOCK' then return end
-	
-	GrimoireTable = {
-		-- [spell name] = {grimoire id by ranks}
-		['Imp'] = {
-			['Spell_Fire_FireBolt']							= {nil,     '16302', '16316', '16317', '16318', '16319', '16320'}, -- Firebolt
-			['Spell_Shadow_BloodBoil']					= {'16321', '16322', '16323', '16324', '16325'}, -- Blood Pact
-			['Spell_Fire_FireArmor']						= {'16326', '16327', '16328', '16329', '16330'}, -- Fire Shield
-			['Spell_Shadow_ImpPhaseShift']			= {'16331'} -- Phase Shift
-		},
-		['Voidwalker'] = {
-			['Spell_Shadow_GatherShadows']			= {nil,     '16346', '16347', '16348', '16349', '16350'}, -- Torment
-			['Spell_Shadow_SacrificialShield']	= {'16351', '16352', '16353', '16354', '16355', '16356'}, -- Sacrifice
-			['Spell_Shadow_AntiShadow']					= {'16357', '16358', '16359', '16360', '16361', '16362'}, -- Consume Shadows
-			['Spell_Shadow_BlackPlague']				= {'16363', '16364', '16365', '16366'} -- Suffering
-		},
-		['Succubus'] = {
-			['Spell_Shadow_Curse']							= {nil,     '16368', '16371', '16372', '16373', '16374'}, -- Lash of Pain
-			['Spell_Shadow_SoothingKiss']				= {'16375', '16376', '16377', '16378'}, -- Soothing Kiss
-			['Spell_Shadow_MindSteal']					= {'16379'}, -- Seduction
-			['Spell_Magic_LesserInvisibilty']		= {'16380'} -- Lesser Invisibility
-		},
-		['Felhunter'] = {
-			['Spell_Nature_Purge']							= {nil,     '16381', '16382', '16383'}, -- Devour Magic
-			['Spell_Shadow_LifeDrain']					= {'16384', '16385', '16386', '16387'}, -- Tainted Blood
-			['Spell_Shadow_MindRot']						= {'16388', '16389'}, -- Spell Lock
-			['Spell_Shadow_AuraOfDarkness']			= {'16390'} -- Paranoia
-		}
+local GrimoireTable = {
+	-- [spell name] = {grimoire id by ranks}
+	['Imp'] = {
+		['Spell_Fire_FireBolt']							= {nil,     '16302', '16316', '16317', '16318', '16319', '16320'}, -- Firebolt
+		['Spell_Shadow_BloodBoil']					= {'16321', '16322', '16323', '16324', '16325'}, -- Blood Pact
+		['Spell_Fire_FireArmor']						= {'16326', '16327', '16328', '16329', '16330'}, -- Fire Shield
+		['Spell_Shadow_ImpPhaseShift']			= {'16331'} -- Phase Shift
+	},
+	['Voidwalker'] = {
+		['Spell_Shadow_GatherShadows']			= {nil,     '16346', '16347', '16348', '16349', '16350'}, -- Torment
+		['Spell_Shadow_SacrificialShield']	= {'16351', '16352', '16353', '16354', '16355', '16356'}, -- Sacrifice
+		['Spell_Shadow_AntiShadow']					= {'16357', '16358', '16359', '16360', '16361', '16362'}, -- Consume Shadows
+		['Spell_Shadow_BlackPlague']				= {'16363', '16364', '16365', '16366'} -- Suffering
+	},
+	['Succubus'] = {
+		['Spell_Shadow_Curse']							= {nil,     '16368', '16371', '16372', '16373', '16374'}, -- Lash of Pain
+		['Spell_Shadow_SoothingKiss']				= {'16375', '16376', '16377', '16378'}, -- Soothing Kiss
+		['Spell_Shadow_MindSteal']					= {'16379'}, -- Seduction
+		['Spell_Magic_LesserInvisibilty']		= {'16380'} -- Lesser Invisibility
+	},
+	['Felhunter'] = {
+		['Spell_Nature_Purge']							= {nil,     '16381', '16382', '16383'}, -- Devour Magic
+		['Spell_Shadow_LifeDrain']					= {'16384', '16385', '16386', '16387'}, -- Tainted Blood
+		['Spell_Shadow_MindRot']						= {'16388', '16389'}, -- Spell Lock
+		['Spell_Shadow_AuraOfDarkness']			= {'16390'} -- Paranoia
 	}
-	self:RegisterEvent('PET_BAR_UPDATE')
-	self:SecureHook('MerchantFrame_UpdateMerchantInfo')
-end
+}
 
-function GrimoireKeeper:PET_BAR_UPDATE()
-	petType = self:GetDemonFamily()
+GrimoireKeeper:RegisterEvent('PET_BAR_UPDATE')
+GrimoireKeeper:SetScript('OnEvent', function()
+	petType = GrimoireKeeper:GetDemonFamily()
 	if not petType then return end
 	
 	GrimoireKeeperData = {}
 	
-	for spellTexture, spellRank in pairs(self:ScanPetSpells()) do
+	for spellTexture, spellRank in pairs(GrimoireKeeper:ScanPetSpells()) do
 		for i=1, spellRank do
 			local GrimoireID = GrimoireTable[petType][spellTexture][i]
 			if GrimoireID then
@@ -50,12 +46,14 @@ function GrimoireKeeper:PET_BAR_UPDATE()
 			end
 		end
 	end
-end
+end)
 
-function GrimoireKeeper:MerchantFrame_UpdateMerchantInfo()
+local oldMerchantFrame_UpdateMerchantInfo = MerchantFrame_UpdateMerchantInfo
+function MerchantFrame_UpdateMerchantInfo()
+	oldMerchantFrame_UpdateMerchantInfo()
 	local _, texture = GetMerchantItemInfo(1)
 	if not petType or texture ~= 'Interface\\Icons\\INV_Misc_Book_06' then return end
-
+	
 	for i=1, MERCHANT_ITEMS_PER_PAGE do
 		local index = (MerchantFrame.page - 1) * MERCHANT_ITEMS_PER_PAGE + i
 		local itemButton = getglobal('MerchantItem'..i..'ItemButton')
@@ -65,7 +63,7 @@ function GrimoireKeeper:MerchantFrame_UpdateMerchantInfo()
 			local link = GetMerchantItemLink(index)
 			if not link then return end
 			local _, _, itemID = string.find(link, 'item:(%d+):')
-			if self:isValidGrimuare(itemID) then
+			if GrimoireKeeper:isValidGrimoire(itemID) then
 				for k in pairs(GrimoireKeeperData) do
 					if itemID == GrimoireKeeperData[k] then -- if grimoire is known
 						SetItemButtonNameFrameVertexColor(merchantButton, 0, .5, .5)
@@ -91,7 +89,7 @@ function GrimoireKeeper:MerchantFrame_UpdateMerchantInfo()
 	end
 end
 
-function GrimoireKeeper:isValidGrimuare(itemID)
+function GrimoireKeeper:isValidGrimoire(itemID)
 	for _, GrimoireIDs in pairs(GrimoireTable[petType]) do
 		for _, GrimoireID in pairs(GrimoireIDs) do
 			if itemID == GrimoireID then return true end
